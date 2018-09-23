c13c2fd20218ff3780f9208f2034ba3338642d1c
Krystyna Szurmańska <kszurmanska@gmail.com>
2018-09-23 18:37:53 +0200

    Update reducers

--- client/modules/Note/Note.js
+++ client/modules/Note/Note.js
@@ -1,30 +1,18 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { PropTypes } from 'react';
+//import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 // Import Style
 import styles from './Note.css';
 
-class Note extends Component {
-  render() {
-    return (
-    );
-  }
-}
+const Note = props =>
+  <li className={styles.Note}>{props.children}</li>;
 
-const mapStateToProps = (state) => {
-  return {};
-};
 
-const mapDispatchToProps = (dispatch) => {
-  return {};
-};
 
 Note.propTypes = {
+  //children: PropTypes.any,
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Note);
+export default Note;
--- client/modules/Note/NoteReducer.js
+++ client/modules/Note/NoteReducer.js
@@ -1,14 +1,32 @@
 // Import Actions
-import {  } from './NoteActions';
+import { CREATE_NOTE, UPDATE_NOTE, DELETE_NOTE, EDIT_NOTE } from './NoteActions';
 
 // Initial State
-const initialState = {};
+const initialState = [];
 
-const NoteReducer = (state = initialState, action) => {
+export default function notes(state = initialState, action) {
   switch (action.type) {
-    default:
-      return state;
-  }
-};
+    case CREATE_NOTE:
+      return [...state, action.note];
 
-export default NoteReducer;
+    case UPDATE_NOTE:
+      return state.map((note) => {
+        return note.id === action.id ? { ...note, ...action.note } : note;
+      });
+
+    case EDIT_NOTE:
+      return state.map((note) => {
+      //  return note.id === action.NoteId ? { ...note, editing: true } : note;
+        if (note.id === action.noteId) {
+          return Object.assign({}, { ...note, editing: false }, { ...note, editing: true });
+        }
+        return note;
+  });
+
+    case DELETE_NOTE:
+  return state.filter((note) => note.id !== action.noteId);
+
+    default:
+  return state;
+}
+}
